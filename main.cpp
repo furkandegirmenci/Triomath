87ed49791525d40c98fe655812f387fd88f8ed11
furkandegirmenci <92688554+furkandegirmenci@users.noreply.github.com>
2023-07-28 11:04:24 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,14 @@
 #include "Number.cpp"
 
 int main() {
-    Trio::Number num1(5), num2(3), num3;
-
-    std::cout << "Birinci sayıyı giriniz :" << num1.get();
+    Trio::Number num1(0), num2, num3;
+    std::cout << "Birinci sayıyı giriniz : ";
     std::cin >> num1;
-
-    std::cout << "\nİkinci sayıyı giriniz : " << num2.get();
+    std::cout << "\nİkinci sayıyı giriniz : ";
     std::cin >> num2;
 
     num3 = num1 + num2;
-    std::cout << "\nİki sayının toplamı : " << num3.get();
+    std::cout << "\n\n\nİki sayının toplamı : " << num3.get();
     num3 = num1 - num2;
     std::cout << "\nİki sayının farkı : " << num3.get();
     num3 = num1 * num2;
@@ -19,7 +17,7 @@ int main() {
     num3 = num1 / num2;
     std::cout << "\nİki sayının bölümü : " << num3.get();
     num3 = num1 ^ num2;
-    std::cout << "\nÜst İşlemi : " << num1.get() << " ^ " << num2.get() << " = " << num3.get();
+    std::cout << "\n\nÜst İşlemi : " << num1.get() << " ^ " << num2.get() << " = " << num3.get();
     num3 = num1 % num2;
     std::cout << "\nMod İşlemi : " << num1.get() << " % " << num2.get() << " = " << num3.get();
     num3 = ++num1;
@@ -29,11 +27,10 @@ int main() {
     num3 = -num1;
     std::cout << "\nBirinci sayının negatif değeri : " << num3.get();
     num3 = ++num2;
-    std::cout << "\nİkinci sayının 10 fazlası : " << num3.get();
+    std::cout << "\n\nİkinci sayının 10 fazlası : " << num3.get();
     num3 = --num2;
     std::cout << "\nİkinci sayının 10 azı : " << num3.get();
     num3 = -num2;
     std::cout << "\nİkinci sayının negatif değeri : " << num3.get() << std::endl;
-
     return 0;
 }
